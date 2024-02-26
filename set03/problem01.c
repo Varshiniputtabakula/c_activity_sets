0a7bb02d10cd02ede610645581b8c32106b3be5d
Varshiniputtabakula <141308002+Varshiniputtabakula@users.noreply.github.com>
2024-02-26 11:20:49 +0530

    test

---
+++ set03/problem01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    
+}
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
